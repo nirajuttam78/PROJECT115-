b380a359898184333450598ec720522d200976f2
nirajuttam78 <104832125+nirajuttam78@users.noreply.github.com>
2023-11-06 17:38:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-function preLoad(){
+lipsX = 0;
+lipsY = 0;
+lipstick = "";
 
+function preload(){
+ lipstick = loadImage("https://i.postimg.cc/KzQPPqZw/image-removebg-preview-17.png");
 }
 
 function setup(){
-    canvas = createCanvas(300, 300);
+    canvas = createCanvas(400, 300);
     canvas.center();
     video = createCapture(VIDEO);
     video.size(300, 300);
@@ -18,7 +22,8 @@ function modelLoaded(){
 }
 
 function draw(){
-    image(video, 0, 0, 300, 300);
+   image(video, 0, 0, 400, 300);
+   image(lipstick, lipsX+25, lipsY+5, 40, 30);
 }
 
 function take_snapshot(){
@@ -29,6 +34,8 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results);
         console.log("nose x = " + results[0].pose.nose.x);
+        lipsX = results[0].pose.nose.x;
         console.log("nose y = " + results[0].pose.nose.y);
+        lipsY = results[0].pose.nose.y;
     }
 }
